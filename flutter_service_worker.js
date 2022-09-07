913c856107fa4d38d7fe85c508767e4a24740783
fitcrewvizag <112107191+fitcrewvizag@users.noreply.github.com>
2022-09-07 22:48:22 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1988a497ce94b7d88d4220b5757ada5f",
-"index.html": "b2ee9519d417ced47c0de00c1f013c3c",
-"/": "b2ee9519d417ced47c0de00c1f013c3c",
+"index.html": "87b8f190c205e4a5f21c8b52c56cf3ee",
+"/": "87b8f190c205e4a5f21c8b52c56cf3ee",
 "main.dart.js": "b0356f59ead61ad4547ca987d40af6c1",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
